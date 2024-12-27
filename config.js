9b773bab3d7c8ccdc4e8dd4ff9936050273db7b3
David Cyril <168988025+DeeCeeXxx@users.noreply.github.com>
2024-12-27 20:24:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); 
 
-global.owner = process.env.OWNER.split(','); 
+global.owner = process.env.OWNER_NUMBER.split(','); 
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.ownername = process.env.OWNER_NAME || "David Cyril";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
